ff85adcd5c02d82aa587d1d83ef072b91051be4d
AbeerAljohanii <abeeralialjohani@gmail.com>
2024-10-28 17:40:58 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>First Commit</p>
+    </div>
+  );
 }
 
 export default App;
